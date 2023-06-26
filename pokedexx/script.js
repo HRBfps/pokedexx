8afeee065a76a8780f33d03e96dd3c606ef00e32
HRB.7 <84103822+HRBfps@users.noreply.github.com>
2023-06-26 19:25:49 -0300

    mudar a pasta

--- pokedexx/script.js
+++
@@ -1,9 +0,0 @@
-let dados = document.querySelector("forms");
-
-dados.addEventListener('submit', function(e){
-    event.preventDefault();
-
-    let peso = document.getElementById("peso").value;
-    let altura = document.getElementById("altura").value;
-    console.log(altura,peso)
-})
