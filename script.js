cf44bd987f2c12d00679c4fb198ff27506e3159b
HRB.7 <84103822+HRBfps@users.noreply.github.com>
2023-06-27 14:35:54 -0300

    fifcou ruim perdaowagner

--- script.js
+++ script.js
@@ -1,9 +1,52 @@
-let dados = document.querySelector("forms");
+let lista = [
+'Arroz: ',
+'Feijão: ',
+'Óleo: ',
+'Macarrão: ',
+'Sal: ',
+'Açucar: ',
+'Farinha de trigo: ',
+'Carnes: ',
+'Frutas: ',
+'Legumes: ',
+'Hortaliças: '
+]
+
+let listap = [
+    15, // Arroz
+    10, // Feijão
+    10, // Óleo
+    5, // Macarrão
+    4, // Sal
+    8, // Açucar
+    6, // Farinha de Trigo
+    50 // Carnes
+    ,12 // Frutas
+    ,12 // Legumes
+    ,12 // Hortaliças
+]
+let infos = document.getElementById("infos")
+let dados = document.querySelector("#forms");
+
+
 
 dados.addEventListener('submit', function(e){
-    event.preventDefault();
+    e.preventDefault();
 
-    let peso = document.getElementById("peso").value;
-    let altura = document.getElementById("altura").value;
-    console.log(altura,peso)
+    let Din = document.getElementById("din").value;
+
+    for(let i=0;i<11;i++) {
+        var p = document.createElement("p")
+        p.textContent = lista[i]+"("+listap[i]+"): "+ Math.floor(Din / listap[i])
+        document.getElementById("infos").appendChild(p);
+    }
+})
+
+dados.addEventListener('reset', function(e){
+    e.preventDefault();
+
+    for(let i=0;i<11;i++) {
+        p.textContent = ""
+        document.getElementById("infos").replaceChild(p);
+    }
 })
